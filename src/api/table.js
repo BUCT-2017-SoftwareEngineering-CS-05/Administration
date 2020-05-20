43368073531ba5e4cbfad36ef12015a8be4fc88f
awen0707 <15040326677@163.com>
2020-05-21 00:31:31 +0800

    11

--- src/api/table.js
+++ src/api/table.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getList(params) {
   return request({
-    url: '/ManageUser/GetUser',
+    url: '/ManageUser/GetUsers',
     method: 'get',
     params
   })
